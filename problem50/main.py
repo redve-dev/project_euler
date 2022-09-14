5339c714509060d4e4d34e66f41fd26a038d9c00
redve <106469071+redve-dev@users.noreply.github.com>
2022-09-14 19:04:18 +0200

    change primality test

--- problem50/main.py
+++ problem50/main.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from time import time
 # https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n/3035188#3035188
 def generate_primes_below_n(n):
     sieve=[ True ]* ( n//2 )
@@ -7,11 +8,8 @@ def generate_primes_below_n(n):
             sieve[ i*i //2 :: i ] = [False] * (( n-i*i-1) //(2*i)+1)
     return [2] + [2*i+1 for i in range(1, n//2) if sieve[i]]
 
-def is_prime_triv(n):
-    for i in range(2, int(sqrt(n))):
-        if n%i == 0:
-            return False
-    return True
+def is_prime(n):
+    return pow(2, n-1, n) == 1
 
 n = 4_000 # sum of 4_000 primes is above 1e6
 nums = generate_primes_below_n(n)
@@ -20,7 +18,7 @@ limit = 1e6
 while True:
     n -= 1
     s = sum(nums[:n]) - 10
-    if is_prime_triv(s):
+    if is_prime(s):
         if s < limit:
             break
 
